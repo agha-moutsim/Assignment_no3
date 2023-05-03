95fac2877116c24d46be103a3f81c2783633ca8a
aghamoutsim <aghamoutsim@gmail.com>
2023-05-03 12:44:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,197 @@
+
+// var arr = ["SSC","HCS","BS","BCOM","PHD"];
+// console.log(arr);
+
+
+//  var names = ["Moustim", "Alica", "Kazuya"];
+//  var scores = [390, 490,400];
+//  var percentages = scores.map(scores => scores / 500 * 100);
+//  for (var i = 0; i < names.length; i++){
+//      console.log(`${names[i]} scored ${scores[i]} marks out of 500, which is ${percentages[i]}%`)
+//  }
+
+
+
+// var cities = ['Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawar'];
+
+// var selectedCities = [];
+// selectedCities.push(cities[0], cities[2], cities[4]);
+
+// console.log(selectedCities);
+
+
+
+//  var color = ["Red", "Blue", "Purple", "Yellow","Black", "White"];
+// var Newcolor = prompt("Enter your color");
+// color.push(Newcolor );
+// console.log(arr);
+
+
+// color.unshift(Newcolor);
+// console.log(color);
+
+
+
+
+// var color = prompt("Enter your color");
+// color.spilt(color);
+// console.log(arr);
+
+
+// color.shift(Newcolor);
+// console.log(color);
+
+// color.pop(Newcolor);
+// console.log(color);
+
+
+// var colors = ["Red", "Green", "Blue", "Yellow", "Orange"]; 
+//  var indexToDelete = prompt("Enter the index to start deleting colors from:");
+//  var numToDelete = prompt("Enter the number of colors to delete:");
+
+// indexToDelete = parseInt(indexToDelete);
+// numToDelete = parseInt(numToDelete);
+// colors.splice(indexToDelete, numToDelete
+
+
+
+
+
+
+//  var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+//  var uniqueArr = [];
+ 
+ 
+//  for (var i = 0; i < arr1.length; i++) {
+ 
+   
+//    if (uniqueArr.indexOf(arr1[i]) === -1) {
+ 
+     
+//      uniqueArr.push(arr1[i]);
+//    }
+//  }
+ 
+//  console.log(uniqueArr);
+ 
+
+
+
+// var a = ["Karachi", "Lahore", "Islamabad", "Faisalabad"];
+// var o = ["th","st","nd","rd"];
+
+// console.log("1"+o[1]+" choice is "+a[1]);
+// console.log("2"+o[2]+" choice is "+a[0]);
+
+// console.log("3"+o[3]+" choice is "+a[2]);
+
+// console.log("4"+o[0]+" choice is "+a[3]);
+
+
+// var a = [10,20,4,40,60,70];
+// var b = [1,2,3,4,5,6,7,8,9,10];
+
+// var union = a.concat(b.filter(function(item) {
+//   return a.indexOf(item) === -1;
+// }));
+
+// console.log(union);
+
+// var aar1 = [10,20,4,40,60,70,20,10,60];
+// var agha = [];
+// for (var i = 0; i < aar1.length; i++) {
+ 
+   
+//     if (agha.indexOf(aar1[i]) === -1) {
+  
+      
+//       agha.push(aar1[i]);
+//     }
+//   }
+  
+//   console.log(agha);
+
+
+// for (var i = 1; i <= 15; i++) {
+//     console.log(i);
+//   }
+
+ 
+//   for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+//   }
+  
+
+//   var evenNumbers = [];
+
+//   for (var i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//       evenNumbers.push(i);
+//     }
+//   }
+  
+//   console.log(evenNumbers);
+  
+
+
+
+// var oddNumbers = [];
+
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     oddNumbers.push(i);
+//   }
+// }
+
+// console.log(oddNumbers);
+
+
+// var a = [10,98,45,67,34];
+// var largestnumber = [];
+// for (var i = 1; i < a.length; i++) {
+ 
+//   if(a[i] > largestnumber){
+//     largestnumber = a[i];
+//   }
+//     }
+  
+
+//   console.log("The largest number is "+largestnumber);
+
+
+//   var a = [2,5,89,34,26,76,98];
+// var smallest = [];
+// for (var i = 1; i > a.smallest; i++) {
+ 
+//   if(a[i] < smallest){
+//     smallest = a[i];
+//   }
+// }
+//   console.log("smallest "+smallest);
+
+
+var bak = ["Paties","Muffins","Cake","Khataiyan"];
+var a = prompt("What are you buying today");
+
+for (var i = 0; i < bak.length; i++){
+  if(bak[i] == a){
+    console.log("item hy")
+
+
+  }
+}
+console.log("item ni hy");
+
+
+
+
+
+
+
+
+
+
+
+
+
